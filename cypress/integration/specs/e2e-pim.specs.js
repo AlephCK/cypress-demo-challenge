0e2261610785ee9a89b4dd125621e274ce09067c
Winston Cruz <alephck@outlook.com>
2022-11-30 19:08:33 -0400

    AC-0001: Fix failing test case & update config.js

--- cypress/integration/specs/e2e-pim.specs.js
+++ cypress/integration/specs/e2e-pim.specs.js
@@ -1,5 +1,5 @@
 import { loginUser } from "../actions/login-actions";
-import { clickMainMenuItem, checkTopbarHeaderText, waitOnRequest } from "../actions/general-actions"
+import { clickMainMenuItem, checkTopbarHeaderText } from "../actions/general-actions"
 import { clickAddEmployeeButton, fillEmployeeFullName, clickSaveButton, validateEmployeeFields, checkPersonalDetails } from "../actions/pim-actions"
 
 context('E2E: Login Tests', () => {
@@ -8,15 +8,23 @@ context('E2E: Login Tests', () => {
     cy.visit(Cypress.env('url'));
     loginUser(Cypress.env('TEST_USER'), Cypress.env('TEST_PASSWORD'));
     checkTopbarHeaderText('Dashboard');
+    clickMainMenuItem('PIM');
+    checkTopbarHeaderText('PIM');
   });
 
   it('Add a new employee', () => {
-    clickMainMenuItem('PIM');
-    checkTopbarHeaderText('PIM');
     clickAddEmployeeButton();
     fillEmployeeFullName();
     clickSaveButton();
     validateEmployeeFields();
     checkPersonalDetails();
   });
+
+  it('Search an employee', () =>{
+
+  })
+
+  it('Delete an employee', () =>{
+
+  })
 })
